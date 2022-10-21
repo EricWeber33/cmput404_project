06bbc2424f6184b50a50c3239986fd9fa08c18f3
OmgPockii <kzhu1@ualberta.ca>
2022-10-20 19:04:55 -0600

    posts WIP (views)

--- mysite/posts/views.py
+++ mysite/posts/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from .serializer import PostSerializer
+from .models import Post
+
 # Create your views here.
 
 class PostDetail(APIView):
