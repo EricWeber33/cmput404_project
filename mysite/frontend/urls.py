c696f3b110949c64bd506560b591350b6b4947b0
alisha03 <crasta@ualberta.ca>
2022-12-02 01:30:23 -0700

    like post frontend

--- mysite/frontend/urls.py
+++ mysite/frontend/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     re_path('^authors/(?P<pk>.+)/home/comment_submit/$', views.comment_submit),
     re_path('^authors/(?P<pk>.+)/home/post_submit/$', views.post_submit),
     re_path('^authors/(?P<pk>.+)/home/$', views.homepage_view),
+    re_path('^authors/(?P<pk>.+)/home/like_post_submit/(?P<post_id>.+)/$', views.like_post_submit),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
--- mysite/frontend/views.py
+++ mysite/frontend/views.py
@@ -9,16 +9,17 @@ from PIL import Image
 from inbox.models import Inbox
 from authors.models import Author
 from authors.serializer import AuthorSerializer
-from posts.models import Post, Comment, Comments
-from posts.serializer import PostSerializer, CommentSerializer
+from posts.models import Post, Comment, Comments, Like
+from posts.serializer import PostSerializer, CommentSerializer, LikeSerializer
 from .forms import PostForm
 import uuid
 import datetime
-import requests 
+import requests
 import json
 import commonmark
 import base64
 from io import BytesIO
+
 
 def get_object_from_url(model, url):
     """attempts to return a db item using a url as primary key"""
@@ -33,7 +34,8 @@ def get_object_from_url(model, url):
             return model.objects.get(pk=url)
         except model.DoesNotExist:
             return None
-        
+
+
 @permission_classes(IsAuthenticated,)
 def post_submit(request, pk):
     url = request.build_absolute_uri()
@@ -60,13 +62,15 @@ def post_submit(request, pk):
                     # supplied an image but didn't say the content type was an image
                     # should this throw an error?
                     pass
-                image.save(buffer, format=file_type) # write the image data to the buffer so it can be encoded
-                content = 'data:'+ content_type + ',' + base64.b64encode(buffer.getvalue()).decode('utf-8')
+                # write the image data to the buffer so it can be encoded
+                image.save(buffer, format=file_type)
+                content = 'data:' + content_type + ',' + \
+                    base64.b64encode(buffer.getvalue()).decode('utf-8')
             post_data = {
                 "csrfmiddlewaretoken": get_token(request),
                 "title": title,
                 "description": description,
-                "content": content, 
+                "content": content,
                 "contentType": content_type,
                 "source": "",
                 "visibility": visibility,
@@ -85,13 +89,14 @@ def post_submit(request, pk):
                 }
                 client.post(post_endpoint, cookies=cookies, data=post_data)
     return HttpResponseRedirect(home_url)
-        
+
+
 @permission_classes(IsAuthenticated,)
 def comment_submit(request, pk):
     url = request.build_absolute_uri()
     home_url = url.split('/home/')[0] + "/home/"
     if request.method == 'POST':
-       
+
         # TODO use the endpoint instead of the model view
         # attempt at this below
         """
@@ -131,13 +136,14 @@ def comment_submit(request, pk):
         inbox.save()
     return HttpResponseRedirect(home_url)
 
+
 @permission_classes(IsAuthenticated,)
 def homepage_view(request, pk):
     url = request.build_absolute_uri().split('home/')[0]
     inbox = Inbox.objects.get(pk=url)
-    # inbox uses a json schema which means updates wont be reflected 
+    # inbox uses a json schema which means updates wont be reflected
     # here we get the items referenced from db and replace them in the items box
-    removal_list = [] # keep track of indexes of deleted inbox items
+    removal_list = []  # keep track of indexes of deleted inbox items
     for i in range(len(inbox.items)):
         if not 'type' in inbox.items[i].keys():
             removal_list.append(i)
@@ -146,7 +152,10 @@ def homepage_view(request, pk):
                 post = Post.objects.get(pk=inbox.items[i]['id'])
                 inbox.items[i] = PostSerializer(post).data
                 if inbox.items[i]['contentType'] == 'text/markdown':
-                    inbox.items[i]['content'] = commonmark.commonmark(inbox.items[i]['content'])
+                    inbox.items[i]['content'] = commonmark.commonmark(
+                        inbox.items[i]['content'])
+                inbox.items[i]['like_count'] = Like.objects.filter(
+                    object=inbox.items[i]['source']).count()
             except Post.DoesNotExist:
                 removal_list.append(i)
         elif inbox.items[i]['type'] == "comment":
@@ -157,3 +166,51 @@ def homepage_view(request, pk):
     inbox.save()
     post_form = PostForm()
     return render(request, 'homepage/home.html', {'type': inbox.type, 'items': inbox.items, "post_form": post_form})
+
+
+@permission_classes(IsAuthenticated,)
+def like_post_submit(request, pk, post_id):
+    
+    post = get_object_from_url(Post, post_id)
+    url = request.build_absolute_uri()
+    home_url = url.split('/home/')[0] + "/home/"
+    post_url = url.split('/authors/')[0] + '/authors/' + post.author.id + '/posts/' + post.id + '/'
+
+    #TODO use the endpoint instead, couldn't get it to work
+    like_obj = Like.objects.all().filter(object=post_url, author=request.user.author)
+    if len(like_obj) == 0:
+        like = Like.objects.create(
+        context=f"TODO",
+        author=request.user.author,
+        summary=f"{request.user.author.displayName} likes your post",
+        object=post_url
+        )
+        like.save()
+        inbox = get_object_from_url(Inbox, post.author.url)
+        inbox.items.insert(0, LikeSerializer(like).data)
+        inbox.save()
+    return HttpResponseRedirect(home_url)
+'''
+    post = get_object_from_url(Post, post_id)
+
+    url = request.build_absolute_uri()
+    home_url = url.split('/home/')[0] + "/home/"
+    post_like_endpoint = post.author.url + 'posts/' + post.id + '/likes/'
+    #post_like_endpoint = post.source + '/likes'
+
+    if request.method == 'POST':
+        with requests.Session() as client:
+            client.headers.update(request.headers)
+            client.headers.update({
+                'Content-Type': None,
+                'Content-Length': None,
+            })
+            cookies = {
+                'sessionid': request.session.session_key,
+                'csrftoken': get_token(request)
+            }
+            client.post(post_like_endpoint, cookies=cookies, headers=headers)
+    
+    return HttpResponseRedirect(home_url)
+
+'''
